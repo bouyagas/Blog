0f9d7677cbabb8c956c28e43f64df1526a582881
mohamed gassama <bouyagassama@gmail.cpm>
2017-05-10 23:36:44 -0400

    cool

--- models/article.js
+++ models/article.js
@@ -1,7 +1,7 @@
 const db = require('../lib/dbConfig.js');
 
 
-const getAllArticles = (req, res, next) => {
+ function getAllArticles(req, res, next){
   db.any(`SELECT * FROM article`)
     .then((article) => {
       res.article = article;
@@ -9,21 +9,21 @@ const getAllArticles = (req, res, next) => {
     })
     .catch(err => next(err));
 
-};
+}
 
-const addArticle = (req, res, next) => {
+function addArticle(req, res, next){
  db.none(`INSERT INTO article (title, content, user_id)
           VALUES ($1, $2, $3);`, [req.body.title, req.body.content, req.body.user_id])
     .then(next())
     .catch(err => next(err));
-};
+}
 
 
-const deleteArticle = (req, res, next) => {
+function deleteArticle(req, res, next){
  db.none(`DELETE FROM article WHERE id = $1;`, [req.params.id])
     .then(next())
     .catch(err => next(err));
-};
+}
 
 
 module.exports = {
